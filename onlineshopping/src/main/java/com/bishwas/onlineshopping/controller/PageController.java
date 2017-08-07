94408424040e9ef5084d08959d5596c510cfcb01
bishwaskalika <bishwas.kalika.c1@gmail.com>
2017-08-07 12:45:18 +0545

    Setup

---
+++ onlineshopping/src/main/java/com/bishwas/onlineshopping/controller/PageController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+/**
+ * @author dell
+ *
+ */
+package com.bishwas.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController{
+	
+	@RequestMapping(value = {"/", "/index", "/home"})
+	public ModelAndView index(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","Welcome 2 Spring web mvc");
+		return mv;
+	}
+	
+}
