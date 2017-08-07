8cde48ff5fc6c49c62ce72f776a0503112165feb
bishwaskalika <bishwas.kalika.c1@gmail.com>
2017-08-07 21:57:07 +0545

    Successfully Loaded Static Resources

--- onlineshopping/src/main/java/com/bishwas/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/bishwas/onlineshopping/controller/PageController.java
@@ -8,7 +8,9 @@
 package com.bishwas.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,6 +20,26 @@ public class PageController{
 	public ModelAndView index(){
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting","Welcome 2 Spring web mvc");
+		return mv;
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam( value="greeting", required=false)String greeting){
+//		if(greeting==null){
+//			greeting="Hello There";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable( "greeting")String greeting){
+		if(greeting==null){
+			greeting="Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
 		return mv;
 	}
 	
